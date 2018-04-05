11257f5a407d34343c5876f75ab2b4a80c54840b
Perrine <morandperrine@hotmail.fr>
2018-04-05 17:43:31 +0200

    Méthode fight

--- app/src/main/java/fr/wcs/hackathon/CombatActivity.java
+++ app/src/main/java/fr/wcs/hackathon/CombatActivity.java
@@ -9,13 +9,16 @@ import android.widget.TextView;
 
 public class CombatActivity extends AppCompatActivity {
 
+    TextView textLifeP1;
+    TextView textLifeP2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_combat);
 
-        final TextView textLifeP1 = findViewById(R.id.text_life_p1);
-        final TextView textLifeP2 = findViewById(R.id.text_life_p2);
+        textLifeP1 = findViewById(R.id.text_life_p1);
+        textLifeP2 = findViewById(R.id.text_life_p2);
 
         ImageView ivP1 = findViewById(R.id.img_player_one);
         ImageView ivP2 = findViewById(R.id.img_player_two);
@@ -26,23 +29,41 @@ public class CombatActivity extends AppCompatActivity {
         textLifeP1.setText(String.valueOf(lifeP1));
         textLifeP2.setText(String.valueOf(lifeP2));
 
-        final int combatP1 = 13;
-        final int combatP2 = 27;
+        final int combatP1 = 3;
+        final int combatP2 = 7;
 
-        Button fight = findViewById(R.id.btn_fight);
+        final HeroModel player1 = new HeroModel(lifeP1, combatP1);
+        final HeroModel player2 = new HeroModel(lifeP2, combatP2);
 
-        fight.setOnClickListener(new View.OnClickListener() {
+        final Button btnFight = findViewById(R.id.btn_fight);
+
+        btnFight.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                fight(player1, player2);
             }
+
         });
 
-
-
-
-        FontHelper.setFont(fight, "pix.ttf");
+        FontHelper.setFont(btnFight, "pix.ttf");
         FontHelper.setFont(textLifeP1, "pix.ttf");
         FontHelper.setFont(textLifeP2, "pix.ttf");
     }
+
+    public void fight(HeroModel player1, HeroModel player2) {
+            textLifeP2.setText(String.valueOf(player2.getDurability()));
+            player2.setDurability(player2.getDurability() - player1.getCombat());
+            if (player2.getDurability() <= 0) {
+                textLifeP2.setText(String.valueOf(0));
+            } else {
+                player1.setDurability(player1.getDurability() - player2.getCombat());
+                textLifeP1.setText(String.valueOf(player1.getDurability()));
+                if (player1.getDurability() <= 0) {
+                    textLifeP1.setText(String.valueOf(0));
+
+            }
+        }
+    }
 }
+
+
